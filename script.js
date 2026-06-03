a819589be46bec66e62067538f80a030d34b01eb
tonystark04v-dev <tonystark04v@gmail.com>
2026-06-03 10:56:59 +0530

    updated subject error

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sendMail(){
     let parms = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
-        subject : document.getElementById("subjec").value,
+        subject : document.getElementById("subject").value,
         message : document.getElementById("message").value,
     }
 
